85688927b77050ecd33defd7bade4ae41f816d2f
Kyle Stephen Andersen <kander78@tesla12.eecs.utk.edu>
2021-10-21 12:42:14 -0400

    Input properly parsed, class implementation started on proj1_rewrite

--- kyle_deliverables/proj1_rewrite.py
+++ kyle_deliverables/proj1_rewrite.py
@@ -1,4 +1,8 @@
-#class Song:
+class Song:
+    def __init__(self, title, time, track):
+        self.title = title
+        self.time = time
+        self.track = track
 #    title
 #    time
 #    track
@@ -22,23 +26,30 @@ fileName = input()
 with open(fileName) as f:
     content = f.readlines()
 
-# Show the file contents line by line.
-# We added the comma to print single newlines and not double newlines.
-# This is because the lines contain the newline character '\n'.
+# split the lines by spaces, assign to proper variable
 for line in content:
     line = line.replace("\n","")
     word = line.split(" ")
     #word = word.replace("_", " ")
-    title = word[0]
-    title = title.replace("_", " ")
-    time_str = word[1]
-    artist_str = word[2]
-    artist_str = artist_str.replace("_", " ")
-    album_str = word[3]
-    album_str = album_str.replace("_", " ")
+    title = word[0].replace("_", " ")
+    #title = title.replace("_", " ")
+    time_str = word[1].replace(":", "")
+    minutes = int(time_str[0])
+    tens = int(time_str[1])
+    ones = int(time_str[2])
+    seconds = (minutes * 60) + (tens * 10) + (ones)
+    artist_str = word[2].replace("_", " ")
+    #artist_str = artist_str.replace("_", " ")
+    album_str = word[3].replace("_", " ")
+    #album_str = album_str.replace("_", " ")
     genre = word[4]
-    track_str = word[5]
-    print(word)
+    track_int = int(word[5])
+
+    #if artist is not already in the map, add
+    #if album not already in the map, add
+    #if song not already in the map, add
+    toAdd = Song(title, seconds, track_int)
+    print(toAdd.time)
 
 
 
