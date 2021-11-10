81518b575ddf80ef01ec52159b497bda593dc3c7
Kyle Stephen Andersen <kander78@tesla12.eecs.utk.edu>
2021-11-10 11:16:22 -0500

    Edited main and gnlp for entity sentiment analysis

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from modules.googleNatrualLanguageProcessing import get_sentiment
+from modules.googleNatrualLanguageProcessing import get_entity_sentiment, get_sentiment
 # from modules.twitter_pull import gen_tweets
 import modules.ReadWrite as rw
 from twitter_pull import gen_tweets
+
 
 def main():
     #gen_tweets("CS students")
@@ -10,12 +11,19 @@ def main():
     ids = {}
     data = {}
 
+    
+
     for id in twitter_data:
         print(id)
         sentiment = get_sentiment(rw.get_value_by_id(twitter_data,id))
+        entsent = get_entity_sentiment(rw.get_value_by_id(twitter_data,id))
         # print("Score: {} Magnitude {}".format(sentiment.score,sentiment.magnitude))
         ids = {"score":sentiment.score,"magnitude":sentiment.magnitude}
         data[id] =  ids
+
+        
+
+
     rw.store_data("data/sentiment_data.json",data)
 
 
--- modules/googleNatrualLanguageProcessing.py
+++ modules/googleNatrualLanguageProcessing.py
@@ -15,4 +15,18 @@ def get_sentiment(text):
 
 def get_entity_sentiment(text):
     """" """
-    return data
+    client = language_v1.LanguageServiceClient() #
+    # following block added by Kyle
+    # attempting entity sentiment analysis
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+    language = "en"
+    document = {"content": text, "type_": type_, "language": language}
+    encoding_type = language_v1.EncodingType.UTF8
+    response = client.analyze_entity_sentiment(request = {'document': document, 'encoding_type': encoding_type})
+    # Loop through entitites returned from the API
+    for entity in response.entities:
+        print(u"Representative name for the entity: {}".format(entity.name))
+        entSentiment = entity.sentiment
+        print(u"Entity sentiment score: {}".format(entSentiment.score))
+        print(u"Entity sentiment magnitude: {}".format(entSentiment.magnitude))# 
+    return response
