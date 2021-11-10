4fbd792e7d30729481f53344a7381621027bb20f
elivols <elivols@gmail.com>
2021-11-10 16:44:10 -0500

    Started to inplement larger twitter pull

--- twitter_pull.py
+++ twitter_pull.py
@@ -10,14 +10,19 @@ def gen_tweets(query):
     auth.set_access_token(twitter_config.ACCESS_TOKEN, twitter_config.ACCESS_SECRET)
     api = tweepy.API(auth)
 
-    #data collection
-    response = api.search_full_archive(label="dataGen", query=query, fromDate=202108101200, maxResults=100)
+    #loop back in time and open and append to data files
+    start_time = 20200000000
+    end_time = 202012010000
     tweets_dict = {}
-    for source in response:
-        tweets_dict[source._json['id_str']] = source._json['text']
+
+    for i in range(start_time, end_time, 3000000):
+        #data collection
+        response = api.search_full_archive(label="dataGen", query=query, fromDate=i, toDate=i+3000000, maxResults=100)
+        for source in response:
+            tweets_dict[source._json['id_str']] = source._json['text']
 
     #data dump
-    with open('data/tweets.json', 'w') as f:
+    with open('data/tweets.json', 'r+') as f:
         json.dump(tweets_dict, f, indent=4)
 
     return 0
